d1cc4b4af9f26602712fa1f080c0cfb979f399c9
Matthew Scott <matthewruelscott@gmail.com>
2016-07-12 16:14:23 -0400

    Made Blitter

---
+++ js/bleets/edit.controller.js
@@ -0,0 +1,24 @@
+"use strict";
+
+(function(){
+  angular
+  .module('bleets')
+  .controller("BleetEditController", [
+    "BleetFactory",
+    "$stateParams",
+    "$state",
+    BleetEditControllerFunction
+  ]);
+
+  function BleetEditControllerFunction(BleetFactory, $stateParams, $state){
+    this.bleet = BleetFactory.get({id: $stateParams.id});
+    this.update = function(){
+      this.bleet.$update({id: $stateParams.id})
+    }
+    this.destroy = function(){
+      this.bleet.$delete({id: $stateParams.id}).then(function(response){
+        $state.go("bleetIndex", ({id: response.id}));
+      });
+    }
+  }
+}());
